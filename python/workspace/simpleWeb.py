fa969a7890811cadeaaa010d7fac7b4482fb31fc
cjp975 <cjp97543@gmail>
2023-04-20 14:26:52 +0900

    sqlite는 autoincrement는 _를 넣지 않고 primarykey는 뒤에 선언 할 수 없다

--- python/workspace/simpleWeb.py
+++ python/workspace/simpleWeb.py
@@ -1,12 +1,136 @@
 from flask import Flask, request, render_template, jsonify, _app_ctx_stack
+import sqlite3
 
+'''
+CREATE TABLE appdata (
+    id INTEGER AUTO_INCREMENT,
+    time datetime DEFAULT current_timestamp,
+    kor INTEGER,
+    math INTEGER,
+    PRIMARY KEY(id)
+)
+'''
+
+DATABASE = 'data.db'
 
 app = Flask(__name__, static_folder='static', template_folder='template')
 
+# 데이터베이스 열기
+def get_db():
+    top = _app_ctx_stack.top
+    if not hasattr(top, 'sqlite_db'):
+        top.sqlite_db = sqlite3.connect(DATABASE)
+    return top.sqlite_db
 
-# @app.route('/')
-# def hello():
-#     return 'Hello'
+# 데이터베이스 정리
+@app.teardown_appcontext
+def close_connection(exception):
+    top = _app_ctx_stack.top
+    if hasattr(top, 'sqlite_db'):
+        top.sqlite_db.close()  
+        
+@app.route('/reset', methods=["POST"])
+def reset():
+    if request.is_json and request.get_json()['API_KEY'] == 'abcd':
+        try:
+            cur = get_db().cursor()
+            cur.execute('DROP TABLE IF EXISTS appdata')
+            cur.execute('CREATE TABLE appdata (id INTEGER PRIMARY KEY AUTOINCREMENT ,time datetime DEFAULT current_timestamp,kor INTEGER, math INTEGER)')
+            get_db().commit()
+        except:
+            get_db().rollback()
+        return "Successfully reset"
+    return "error"
+
+@app.route('/insert', methods=["POST"])
+def insert():
+    data = request.get_json()
+    kor = data['kor']
+    math = data['math']
+    
+    print("kor = {0}".format(data['kor']))
+    print("math = {0}".format(data['math']))
+    
+    try:
+        cur = get_db().cursor()
+        cur.execute('INSERT INTO appdata (kor, math) VALUES (?, ?)', (kor, math))
+        get_db().commit()
+    except:
+        get_db().rollback()
+        return "rollback"
+    
+    return jsonify(code=1)
+
+# 데이터베이스 처리 결과를 딕셔너리 형태로 반환
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+
+@app.route('/select')
+def select():
+    result = []
+    data = request.get_json()
+   
+    if request.is_json and request.get_json()['API_KEY'] == 'abcd':
+        try:
+            get_db().row_factory = dict_factory
+            cur = get_db().cursor()
+            cur.execute('SELECT  time, kor, math FROM appdata')
+            result = cur.fetchall()
+            print(result)
+        except:
+            get_db().rollback()
+    return 'ok'
+
+@app.route('/selectPost', methods=["POST"])
+def selectPost():
+    result = []
+    data = request.get_json()
+    id = data['id']
+    
+    if request.is_json and request.get_json()['API_KEY'] == 'abcd':
+        print(id)
+        print(request.get_json()['API_KEY'])
+        try:
+            get_db().row_factory = dict_factory
+            cur = get_db().cursor()
+            cur.execute(
+                '''
+                SELECT 
+                    id, time, kor, math 
+                FROM
+                    appdata
+                where 
+                    id = ?
+                '''
+                ,(id,) )
+            result = cur.fetchall()
+            print(result)
+        except:
+            get_db().rollback()
+            return "rollback"
+    return result
+
+@app.route('/welcome')
+def welcom():
+    result = []
+    try:
+        get_db().row_factory = dict_factory
+        cur = get_db().cursor()
+        cur.execute('SELECT  time, kor, math FROM appdata')
+        result = cur.fetchall()
+        print(result)
+    except:
+        get_db().rollback()
+    return render_template('/select.html', data=result)
+
+
+@app.route('/')
+def hello():
+    return 'Hello'
 
 @app.route('/hi')
 def hi():
@@ -15,6 +139,26 @@ def hi():
 @app.route('/sample')
 def sample():
     return render_template('/sample.html')
+
+
+@app.route("/add", methods=["post", "get"])
+def add():
+    a = 0
+    b = 0
+    # a = request.args.get('a', None, int)
+    # b = request.args.get('b', None, int)
+    if request.method=="GET":
+        a = int(request.args.get('a'))
+        b = int(request.args.get('b'))
+    
+    if request.method=="POST":
+        # a = int(request.form['a'])
+        # b = int(request.form['b'])
+        data = request.get_json()
+        a = int(data['a'])
+        b = int(data['b'])
+        
+    return str(a+b)
 
 
 if __name__ == '__main__':
